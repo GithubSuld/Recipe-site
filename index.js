4afc7bed58d477a1138209ce32876a023fae3d73
GithubSuld <exclusive.suld@gmail.com>
2024-07-25 16:48:49 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+window.addEventListener('scroll', () => {
+    const aboutButton = document.querySelector('.about-button');
+    const newSection = document.querySelector('.new-section');
+    const aboutButtonOffset = aboutButton.getBoundingClientRect().top + window.scrollY;
+    const scrollPosition = window.scrollY + window.innerHeight;
+    
+    if (scrollPosition > aboutButtonOffset + 200) {  
+        newSection.classList.add('visible');
+    } else {
+        newSection.classList.remove('visible');
+    }
+});
